791debbcfaf340539e8d5a21b13f2dad2fbbb081
dipakp-logicrays <dipakp@logicrays.com>
2023-08-18 10:40:29 +0530

    Magento cardknox plugin inital commit

---
+++ view/frontend/web/js/view/payment/method-renderer/cardknox-google-pay-method.js
@@ -0,0 +1,182 @@
+define([
+    'Magento_Checkout/js/view/payment/default',
+    'Magento_Checkout/js/model/quote',
+    'CardknoxDevelopment_Cardknox/js/view/payment/method-renderer/cardknox-google-pay',
+    'ifields',
+    'Magento_Checkout/js/model/payment/additional-validators',
+    "jquery",
+    "ko",
+    'Magento_Checkout/js/action/redirect-on-success',
+    'Magento_Checkout/js/action/place-order'
+], function (
+    Component,
+    quote,
+    cardknoxGpay,
+    ifields,
+    additionalValidators,
+    $,
+    ko,
+    redirectOnSuccessAction,
+    placeOrderAction
+) {
+    'use strict';
+    window.checkoutConfig.reloadOnBillingAddress = true;
+    const METHOD_ID = 'cardknox_google_pay';
+        
+    return Component.extend({
+        defaults: {
+            template: 'CardknoxDevelopment_Cardknox/payment/cardknox-google-pay-method.html',
+            redirectAfterPlaceOrder: true,
+            grandTotalAmount: 0,
+            paymentMethodNonce: null,
+            xAmount: null
+        },
+        isAllowDuplicateTransaction: ko.observable(false),
+        /**
+         * @return {exports}
+         */
+        initialize: function () {
+            this._super();
+
+            return this;
+        },
+
+        /**
+         * Google pay place order method
+         */
+        startPlaceOrder: function (nonce, xAmount) {
+            this.xAmount = xAmount ;
+            this.setPaymentMethodNonce(nonce);
+            this.isPlaceOrderActionAllowed(true);
+            this.placeOrder();
+        },
+
+        /**
+         * Save nonce
+         */
+        setPaymentMethodNonce: function (nonce) {
+            this.paymentMethodNonce = nonce;
+        },
+
+        getCode: function () {
+            return METHOD_ID;
+        },
+
+        /**
+         * Get data
+         *
+         * @returns {Object}
+         */
+        getData: function () {
+            var data = {
+                'method': this.getCode(),
+                'additional_data': {
+                    'xCardNum': this.paymentMethodNonce,
+                    'xAmount': this.xAmount,
+                    'isSplitCapture': window.checkoutConfig.payment.cardknox_google_pay.isGPaySplitCaptureEnabled,
+                    'xPaymentAction': window.checkoutConfig.payment.cardknox_google_pay.xPaymentAction,
+                    'isAllowDuplicateTransaction': this.getAllowDuplicateTransactionGpay()
+                }
+            };
+            data['additional_data'] = _.extend(data['additional_data'], this.additionalData);
+            return data;
+        },
+
+        initFrame: function () {        
+            if (/[?&](is)?debug/i.test(window.location.search)){
+                setDebugEnv(true);
+            }
+
+            cardknoxGpay.init(this);
+        },
+        /**
+         * @return {Boolean}
+         */
+         validate: function () {
+            return true;
+        },
+        
+        additionalValidator: function () {
+            return additionalValidators.validate();
+        },
+        getAllowDuplicateTransactionGpay: function () {
+            var isAllowDuplicateTransactionGpay = false;
+            if ($('#is_allow_duplicate_transaction_gpay').length) {
+                if($("#is_allow_duplicate_transaction_gpay").prop('checked') == true){
+                    isAllowDuplicateTransactionGpay = true;
+                } else {
+                    isAllowDuplicateTransactionGpay = false;
+                }
+            }
+            return isAllowDuplicateTransactionGpay;
+        },
+        /**
+             * @return {*}
+             */
+        getPlaceOrderDeferredObject: function () {
+            return $.when(
+                placeOrderAction(this.getData(), this.messageContainer)
+            );
+        },
+        /**
+         * Place order.
+         */
+        placeOrder: function (data, event) {
+            var self = this;
+
+            if (event) {
+                event.preventDefault();
+            }
+
+            if (this.validate() &&
+                additionalValidators.validate() &&
+                this.isPlaceOrderActionAllowed() === true
+            ) {
+                this.isPlaceOrderActionAllowed(false);
+
+                this.getPlaceOrderDeferredObject()
+                    .done(
+                        function () {
+                            self.afterPlaceOrder();
+
+                            if (self.redirectAfterPlaceOrder) {
+                                redirectOnSuccessAction.execute();
+                            }
+                        }
+                    ).always(
+                        function () {
+                            self.isPlaceOrderActionAllowed(true);
+                        }
+                    ).fail(
+                        function (response) {
+                            self.isPlaceOrderActionAllowed(true);
+
+                            var error_message = "Unable to process the order. Please try again.";
+                            if (response && response.responseJSON && response.responseJSON.message) {
+                                error_message = response.responseJSON.message;
+                            }
+                            self.showPaymentError(error_message);
+                            if (error_message == 'Duplicate Transaction') {
+                                self.isAllowDuplicateTransaction(true);
+                            } else {
+                                self.isAllowDuplicateTransaction(false);
+                            }
+                        }
+                    );;
+
+                return true;
+            }
+
+            return false;
+        },
+        showPaymentError: function (message) {
+            $(".gpay-error").html("<div> "+message+" </div>").show();
+            setTimeout(function () { 
+                $(".gpay-error").html("").hide();
+            }, 5000);
+            
+            fullScreenLoader.stopLoader();
+            $('.checkout-cart-index .loading-mask').attr('style','display:none');
+        }
+    });
+});
